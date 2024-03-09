2f9336f114a022d2139b618d681111452fc3278e
Vedant_0 <141428937+vedantVerm@users.noreply.github.com>
2024-03-09 18:45:31 +0000

    01_variable

---
+++ .devcontainer/javascipt/01_variable.js
@@ -0,0 +1,13 @@
+const account_id = 144553;
+let accountemail = "vermavedant063@gmail.com";
+var account_password = "12345";
+accoujntcity = "Rehan";
+
+//account_id = 2 ; 
+// const not allowed to cahnged 
+// console.log(account_id);
+accountemail = "vedant063@gmail.com";
+account_password = "32145";
+accoujntcity = "banglore";
+
+console.table([accountemail, account_password , accoujntcity]);
