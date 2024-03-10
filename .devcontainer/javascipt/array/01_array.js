b20495c9e174c32b7150ca310e34a0ed202f28ac
Vedant_0 <141428937+vedantVerm@users.noreply.github.com>
2024-03-10 18:42:41 +0000

    .1_array.js

---
+++ .devcontainer/javascipt/array/01_array.js
@@ -0,0 +1,15 @@
+// array 
+const myarr = [0 , 1 , 2 , 3 , 5 ,4 ,6 , 7,8 ,9];
+// console.log(myarr[0]);
+// shallow copy share the value of the same referance point 
+// deep cpy--> donot share the value of the same reference point 
+
+// Array Method----->
+// myarr.push(10);
+// console.log(myarr)
+
+// myarr.unshift(9);
+// console.log(myarr);
+// myarr.shift();
+// console.log(myarr);
+console.log(myarr.indexOf(5));
