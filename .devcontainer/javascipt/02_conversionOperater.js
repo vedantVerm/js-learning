ab466e12c91862f38a5a7804edd58a82b0510bbd
Vedant_0 <141428937+vedantVerm@users.noreply.github.com>
2024-03-10 07:39:41 +0000

    03_conversion_operator

---
+++ .devcontainer/javascipt/02_conversionOperater.js
@@ -0,0 +1,23 @@
+let score = null ; 
+// console.log(typeof(score));
+// convert into the no 
+let validno = Number(score);
+// console.log(typeof(validno));
+// console.log(validno);
+
+// "33"--> 33
+// "33abc"==> NaN
+// true==>1; false = 0 
+
+// boolen coversion 
+// 1==> true 0==>false 
+// "" false 
+// "vedant" ==> true 
+let isloginin = 1 ; 
+let boolenLogin = Boolean(isloginin)
+console.log(boolenLogin);
+// string conversion 
+
+let string_no = 33;
+let constring = String(string_no);
+console.log(typeof(constring));
