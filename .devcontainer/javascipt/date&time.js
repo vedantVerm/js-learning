11ce7b017bb56d1d2efac01944e8bea8dabbc27e
Vedant_0 <141428937+vedantVerm@users.noreply.github.com>
2024-03-10 17:53:14 +0000

    .devcontainer/javascipt/date&time.js

---
+++ .devcontainer/javascipt/date&time.js
@@ -0,0 +1,21 @@
+let mydate = new Date() ; 
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+
+console.log(typeof mydate);
+
+// year month date 
+
+// let createmydate = new Date(2024 , 0 , 24);
+// console.log(createmydate.toString());
+
+// month date year 
+// let my_createdate = new Date("01-21-2023");
+// console.log(my_createdate.toString());
+
+
+// *******************Timestamp********************
+
+let my_timestamp  = Date.now() ; 
+console.log(my_timestamp);
